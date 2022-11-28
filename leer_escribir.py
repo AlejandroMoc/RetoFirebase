171729548ed33fc56ca40e123807f2a9bdef1eac
A01736353 <118225979+A01736353@users.noreply.github.com>
2022-11-28 13:54:04 -0600

    Update leer_escribir.py

--- leer_escribir.py
+++ leer_escribir.py
@@ -5,6 +5,7 @@ https://codeloop.org/python-firebase-course-for-beginners/
 '''
 import pyrebase
 
+# Cofiguración específica para el Firebase
 config = {
 "apiKey": "AIzaSyCkZtABLrWDkAvyqf8IRr5s3kIwp724vXo",
 "authDomain": "corintios-5023e.firebaseapp.com",
@@ -17,22 +18,31 @@ config = {
 }
 
 firebase = pyrebase.initialize_app(config)
-#accesing database in firebase
+#Accediendo a la base de datos de Firebase
 db = firebase.database()
+# Declarar a la base de datos de firebase como "db"
 
-#reading some atributes of the onKey elements
+
+# Leer los atributos de los elementos
+
+# Obtener todos los datos de la aplicación
 all_users = db.child("app_inventor").get()
 i = 0
+# Imprimir cada uno de los datos
+# Si el index del dato es 4, no imprimirlo y guardar su valor en "num"
+# Este es el dato del número
 for users in all_users.each():
     if (i == 4):
         break
     print(users.val())
     i = i + 1
 num = int(users.val())
+
+# Imprimir el valor del número (num)
 print("Valor de Número: ")
 print(num)
 
-#undating "usuario" data
+# Si el número es mayor a 9, se actualiza la aplicación para mencionarlo.
 if (num > 9):
     db.child("app_inventor").update({"Usuario":"Elemento mayor a 9"})
     print("Actualizado")
